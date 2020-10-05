8d00b590ddcc27be86ece455f3f50ee4f51e6f61
William Grigor <bjgrigor@gmail.com>
2020-10-05 14:53:39 -0500

    Added some basic comments and rearranged imports

--- backend/app.py
+++ backend/app.py
@@ -9,25 +9,31 @@ from .api import api_blueprint
 from .shared import db, redis, jwt
 from .schemas import ma
 
+# Load generic config options
 with open('base_config.yaml') as f:
     basic_conf = yaml.load(f, yaml.BaseLoader)
 
+# Load secret keys
 with open('secret_config.yaml') as f:
     secret_conf = yaml.load(f, yaml.BaseLoader)
 
+# Create Flask app and set config values
 app = Flask(__name__)
 app.config.update(basic_conf)
 app.config.update(secret_conf)
 TOKEN_STORE_EXPIRES_AFTER = timedelta(minutes=15)
 
+# Register callbacks for JWT manager
 jwt.user_identity_loader(lambda user: user.username)
 jwt.user_loader_callback_loader(lambda identity: User.query.filter_by(username=identity).first())
 jwt.token_in_blacklist_loader(lambda decoded: redis.exists(decoded['jti']))
 
+# Connect app with plugins
 db.init_app(app)
 redis.init_app(app)
 jwt.init_app(app)
 ma.init_app(app)
 migrate = Migrate(app, db)
 
+# Mount the API blueprint to the app
 app.register_blueprint(api_blueprint, url_prefix='/api')
